1fe5fcef6bca3ce10f2c4fba8f8af0f54a1787a9
shivam <shivam@DESKTOP-BT1O840>
2023-09-04 11:11:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class MyBaseClass
+{
+    protected void ProtectedMethod()
+    {
+        Console.WriteLine("This is a protected method in MyBaseClass.");
+    }
+}
+class MyDerivedClass : MyBaseClass
+{
+    public void AccessProtectedMethod()
+    {
+        Console.WriteLine("Calling the protected method from MyDerivedClass.");
+        ProtectedMethod();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyDerivedClass derivedObject = new MyDerivedClass();
+
+        derivedObject.AccessProtectedMethod();
+    }
+}
